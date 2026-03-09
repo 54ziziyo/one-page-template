39026b78550356bd2ff1f2f91a53ef87553559fc
54ziziyo <114908712+54ziziyo@users.noreply.github.com>
2026-03-09 23:14:14 +0900

    Add files via upload

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-01-01',
+  devtools: { enabled: true },
+  pages: true,
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    },
+  },
+});
